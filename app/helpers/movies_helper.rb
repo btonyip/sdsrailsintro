f1224e6ac99fea5c9e0b8942bf2dc3118622ab6c
Yip Ze Sheng Brighton Brighton <1005976@mymail.sutd.edu.sg>
2023-06-18 12:25:27 +0000

    part 3 complete

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,16 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+
+  def headercolour(column_name)
+     if @column_sorting == nil
+      ""
+     elsif column_name == @column_sorting.first
+      'hilite bg-warning'
+     else
+      ""
+     end
+   end
+
+  
 end
